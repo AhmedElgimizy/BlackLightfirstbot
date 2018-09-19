883b5017e51bede98de3dd1e1f1ff338eda8d265
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-19 17:30:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -427,8 +427,6 @@ const fs = require('fs');
 
 const gif = require("gif-search");
 
-const client = new Discord.Client({disableEveryone: true});
-
 const prefix = "!";
 /////////////////////////
 ////////////////////////
