fa6cd4ce605b6d3f479a58294316ee8fe20a9cd4
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-19 23:26:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = 'BL!p'
+const prefix = 'BL!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -27,7 +27,7 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-const adminprefix = "BL!help";
+const adminprefix = "BL!";
 const devs = ['ايديك','ايدي اخويك'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
