a0fe87c8e2682b855964d423b5057153e0acc98f
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-19 18:37:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const prefix = 'BL!help'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
