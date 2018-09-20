36111870311c0177f8bf09cf81dde7791eecfa66
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-20 13:25:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -200,4 +200,16 @@ if (message.content.startsWith(prefix + 'help')) { //ALPHACODES
     }
 });
 
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
+    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setColor('RANDOM')
+    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+var channel =member.guild.channels.find('name', 'chat name ? ')
+if (!channel) return;
+channel.send({embed : embed});
+});
+
 client.login(process.env.BOT_TOKEN);
