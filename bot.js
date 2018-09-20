67f173ea979a1ae8a7559e39b888c383daacadf2
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-20 21:18:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -643,4 +643,20 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message => {
+    if(!message.channel.guild) return;
+if (message.content.startsWith('BL!ping')) {
+if(!message.channel.guild) return;
+var msg = `${Date.now() - message.createdTimestamp}`
+var api = `${Math.round(client.ping)}`
+if (message.author.bot) return;
+let embed = new Discord.RichEmbed()
+.setAuthor(message.author.username,message.author.avatarURL)
+.setColor('RANDOM')
+.addField('**Time Taken:**',msg + " ms 📶 ")
+.addField('**WebSocket:**',api + " ms 📶 ")
+message.channel.send({embed:embed});
+}
+});
+
 client.login(process.env.BOT_TOKEN);
