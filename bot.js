4165544be005011f71d99244a98be769ecdc1793
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-21 16:07:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const points = require('./points.json')
-const Discord = require('discord.js');
 
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = 'BL!'
 
