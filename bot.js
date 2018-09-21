bc71bad94534d76ed661f18e21060d9febe8b8e8
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-22 02:02:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -805,30 +805,4 @@ client.on("message", message => {
       }
       });
 
-  const arraySort = require('array-sort'),
-table = require('table');
-
-client.on('message' , async (message) => {
-
-    if(message.content.startsWith(prefix + "topinvite")) {
-
-  let invites = await message.guild.fetchInvites();
-
-    invites = invites.array();
-
-    arraySort(invites, 'uses', { reverse: true });
-
-    let possibleInvites = [['الدعوات', 'الاشخاص']];
-    invites.forEach(i => {
-      possibleInvites.push([i.inviter.username , i.uses]);
-    })
-    const embed = new Discord.RichEmbed()
-    .setColor(0x7289da)
-    .setTitle("دعوات السيرفر")
-    .addField(' المتصدرين' , `${table.table(possibleInvites)}`)
-
-    message.channel.send(embed)
-    }
-});
-
 client.login(process.env.BOT_TOKEN);
