5d2488173579220639ab429adefd13b9347b79dc
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-21 15:21:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -746,4 +746,19 @@ client.on("message", msg => {
   }
 });
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
 client.login(process.env.BOT_TOKEN);
