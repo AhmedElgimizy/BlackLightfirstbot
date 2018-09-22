a52b20911ea6e121e23308dce28b9ad65e7c503c
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-22 23:35:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -912,4 +912,53 @@ client.on('message', async message => {
   }
 });
 
+
+const translate = require('google-translate-api'); //npm i google-translate-api
+ 
+client.on("message", message => {
+ 
+if(message.content.startsWith("BL!translate")) {
+if(message.author.bot) return;
+if(!message.channel.guild)return;
+ 
+let args = message.content.split(' ').slice(1);
+ 
+    if (args[0]) {
+        let from_language = "auto"
+        let to_language = "en"
+        let tobe_translated = message.content.slice(prefix.length + command.length + 1)
+        if (args[0].startsWith("from:")) {
+            from_language = args[0].slice(5)
+            tobe_translated = tobe_translated.slice(args[0].length + 1)
+            if (args[1].startsWith("to:")) {
+                to_language = args[1].slice(3)
+                tobe_translated = tobe_translated.slice(args[1].length + 1)
+            }
+        } else if (args[0].startsWith("to:")) {
+            to_language = args[0].slice(3)
+            tobe_translated = tobe_translated.slice(args[0].length + 1)
+            if (args[1].startsWith("from:")) {
+                from_language = args[1].slice(5)
+                tobe_translated = tobe_translated.slice(args[1].length + 1)
+            }
+        }
+        translate(tobe_translated, {
+            from: from_language,
+            to: to_language
+        }).then(res => {
+            from_language = res.from.language.iso
+            if (res.from.text.value) tobe_translated = res.from.text.value
+            final_text = res.text
+            message.channel.send(`:u7981: | **Translated**.
+**- From**: \`${from_language}\` **|** ${tobe_translated}
+**- To**: \`${to_language}\` **|** ${final_text}`)
+        }).catch(err => {
+            message.channel.send(":x: Usage: `" + prefix + "translate [from:iso] [to:iso] <some text>` \nThe from: and to: are optional, you can check out <http://bit.ly/ISO_codesWiki> for the iso codes\nExample: ```" + prefix + "translate from:ro to:fr Salut, ce mai faci?```")
+        });
+    } else {
+        message.channel.send(":x: Usage: `" + prefix + "translate [from:iso] [to:iso] <some text>` \nThe from: and to: are optional, you can check out <http://bit.ly/ISO_codesWiki> for the iso codes\nExample: ```" + prefix + "translate from:ro to:fr Salut, ce mai faci?```")
+    }
+}
+})
+
 client.login(process.env.BOT_TOKEN);
