84d823898c82d5e6ca88e86e85e75fe7785eda70
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-22 14:23:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -805,57 +805,5 @@ client.on("message", message => {
       }
       });
 
-JavaScript
-var cats = [
-
-"https://cdn.discordapp.com/attachments/479044877745782801/479045993132720128/download_1.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046003937247253/download_2.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046011956887552/download_3.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046019904962562/download_4.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046030214692864/download_5.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046039794352151/download_6.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046055581974549/download_7.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046065367154712/download.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046084442980354/images_1.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046105674547216/images_4.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046113945583639/images_5.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046119838580756/images_6.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046131796672513/images_7.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046143318425621/images_8.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046152491368448/images_9.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046158979825686/images_10.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046162247188498/images_11.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046196434960416/images_12.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046214571130882/images_13.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046219528929290/images_14.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479072892022161408/images_15.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046222045511685/images_16.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046225086382081/images_18.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046228047560736/images_19.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046230794829834/images_20.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046233533710336/images_21.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479074274850766863/images_22.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046289477337089/images_23.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046293436629041/images_24.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046294925606914/images_25.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046303330992169/images_26.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046307072311317/images_27.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046315100340224/images_28.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046325619392537/images_29.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046333114875907/images_30.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046334825889807/images_31.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046338718466058/images_32.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046340379148288/images_33.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046342795198484/images_34.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046349644365827/images.jpg"
-]
-    client.on('message', message => {
-        var args = message.content.split(" ").slice(1);
-    if(message.content.startsWith(prefix + 'هل تعلم')) {
-         var cat = new Discord.RichEmbed()
-.setImage(cats[Math.floor(Math.random() * cats.length)])
-message.channel.sendEmbed(cat);
-    }
-});
 
 client.login(process.env.BOT_TOKEN);
