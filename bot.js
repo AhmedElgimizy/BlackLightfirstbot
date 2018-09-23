be141e0df95ea2732eacf2c258cb98212862f91b
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-24 00:56:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1100,6 +1100,21 @@ client.on('message', message => {
      }
        });
   
+client.on('message', async message => {
+  if(message.content.startsWith(prefix + "اقتراح")) {
+  await  message.channel.send(`اكتب اقتراحك الان`)
+    let filter = m => m.author.id === message.author.id
+      var text = '';
+        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
+          .then(co => {
+            text = co.first().content
 
+              message.channel.send(`تم حفظ اقتراحك الرجاء انتضار الرد من قبل الاداره`)
+                client.channels.get("493540753734369280").send(`${message.author.username}'s sug => ${text}`)
+
+              })
+            }
+          })
+  
 
 client.login(process.env.BOT_TOKEN);
