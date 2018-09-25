fb4a266f6aa7a72c2542bccd0283711b8950e3ee
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-26 01:15:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1249,4 +1249,20 @@ client.on('message', async message => {
   }
 });
 
+@here
+client.on("message", message => {
+    var prefix = "BL!"
+    if (!message.content.startsWith(prefix)) return;
+      let command = message.content.split(" ")[0];
+      command = command.slice(prefix.length);
+        if(command === "mc3d") {
+                const args = message.content.split(" ").slice(1).join(" ")
+        if (!args) return message.channel.send("** Type your skin name **");
+        const image = new Discord.Attachment(`https://visage.surgeplay.com/full/256/${args}`, "skin.png");
+    message.channel.send(image)
+        }
+    });
+
+ 
+ 
 client.login(process.env.BOT_TOKEN);
