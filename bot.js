988377ad52b8bf78fde82ae92d96dde610b1ab42
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-26 14:25:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1262,6 +1262,26 @@ client.on("message", message => {
         }
     });
 
- 
+ client.on("message", message => { //clear
+              var args = message.content.substring(prefix.length).split(" ");
+              if (message.content.startsWith(prefix + "clear")) {
+                  if(!message.channel.guild) return message.reply('**❌ اسف لكن هذا الامر للسيرفرات فقط **');         
+     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**⚠  لا يوجد لديك صلاحية لمسح الشات**');
+          var msg;
+          msg = parseInt();
+        
+        message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+        message.channel.sendMessage("", {embed: {
+          title: "``تــم مسح الشات ``",
+          color: 0x5016f3, 
+          footer: {
+            
+          }
+        }}).then(msg => {msg.delete(3000)});
+                            }
+  
+       
+  });
+
  
 client.login(process.env.BOT_TOKEN);
