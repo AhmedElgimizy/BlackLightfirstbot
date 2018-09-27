d778e18ca77515020930eeecd1a943e3da3071c3
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-27 16:53:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1402,5 +1402,30 @@ message.channel.sendMessage('**الرجاء الانتظار ريث ما يتم 
 }
 });
 
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 100,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription(" ✅    تم ارسال الرابط على الخاص  ")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+                .setAuthor(message.guild.name, message.guild.iconURL)
+        .setDescription(`
+**
+
+-${message.guild.name}  Link
+**`)
+      message.author.sendEmbed(Embed11)
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
