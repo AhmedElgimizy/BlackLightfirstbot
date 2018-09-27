f9747c5b99128f085053d8b8ad4593fb8f80845b
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-27 17:00:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1408,6 +1408,7 @@ client.on('message', message => {
     if(!message.channel.guild) return message.channel.send('This Command Only For Servers !')
             if (!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(**${message.author.username} You Dont Have** ``MANAGE_ROLES`` **Premission**);
 
+      
                      message.guild.createRole({ name: "Owner", color: "#ffffff", permissions: [] })
                      message.guild.createRole({ name: "Co-Owner", color: "#ffffff", permissions: [] })
                      message.guild.createRole({ name: "Leader", color: "#ffffff", permissions: [] })
