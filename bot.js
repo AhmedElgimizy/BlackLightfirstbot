ae7ba0d7ee3a2b361d6f7e38b91130441b50db33
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-27 16:34:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1370,7 +1370,7 @@ const embed = new Discord.RichEmbed()
 });
 
 client.on('message', message => {
-    if (message.content === "BL!") {
+    if (message.content === "BL!serooms") {
     if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
             if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
 
