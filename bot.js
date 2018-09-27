6294aa704f489d2170110a3db93380daf170e8a1
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-09-27 15:57:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1305,6 +1305,20 @@ message.channel.send(embed)
 
 });
 
+client.on('message', message =>{
 
+    if(message.content == "BL!roles"){
+        var 
+        ros=message.guild.roles.size,
+        data = [['Rank', 'RoleName']]
+        for(let i =0;i<ros;i++){
+            if(message.guild.roles.array()[i].id !== message.guild.id){
+         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
+        }}
+        let res = AsciiTable.table(data)
+
+        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
