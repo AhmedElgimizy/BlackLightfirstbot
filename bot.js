5997f78c0468c95bc351203e41a2a691beee00cc
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-10-04 18:31:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1472,4 +1472,35 @@ msg.reply('https://cdn.discordapp.com/avatars/347379495528824832/53c7bed44f53256
               }, 1)
 });
 
+client.on('message', msg => {
+var prefix = "BL!";
+  if(!msg.guild) return;
+    if (msg.content.startsWith(prefix +'tt')) {
+     let args = msg.content.split(" ").slice(1);
+    if(!msg.channel.guild) return msg.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+let ra3d = new Discord.RichEmbed()
+.setThumbnail(msg.author.avatarURL)
+.setDescription(`ان كنت تريد انشاء شات كتابي اظغط علي الايموجي✏`)                                                                                                                                                                       
+.setFooter('لديك دقيقه للاختيار')
+msg.channel.send(ra3d).then(message => {
+    
+    
+ message.react('✏').then(r=>{
+                              
+    
+ let Text = (reaction, user) => reaction.emoji.name === '✏' && user.id === msg.author.id;
+
+ let tt  = message.createReactionCollector(Text, { time: 60000 });
+
+tt.on("collect", r => {
+msg.guild.createChannel(args.join(' '), 'text');
+    msg.channel.send(`☑ تم انشاء شات كتابي بنجاح : \`${args}\``)
+    msg.delete();
+})
+})
+})
+}
+});
+
+
 client.login(process.env.BOT_TOKEN);
