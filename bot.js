0acc4b77e3525002553bc232a61ae6d0b6e75d4c
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-10-11 17:53:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -485,33 +485,6 @@ client.on("message", (message) => {
  
 });
 
-client.on('message', message => {
-    var prefix = "BL!";
-    
-        if (message.author.id === client.user.id) return;
-        if (message.guild) {
-       let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc') {
-        if (!args[1]) {
-    message.channel.send("**$bc <message>**");
-    return;
-    }
-            message.guild.members.forEach(m => {
-       if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                var bc = new Discord.RichEmbed()
-                .addField('» السيرفر :', `${message.guild.name}`)
-                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-                .addField(' » الرسالة : ', args)
-                .setColor('#ff0000')
-                // m.send(`[${m}]`);
-                m.send(`${m}`,{embed: bc});
-            });
-        }
-        } else {
-            return;
-        }
-    });
 
 client.on('message',function(message) {
   if (message.author.bot) return;
