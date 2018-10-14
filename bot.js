ca9d81480f3f309aa156304d100eb21f4b992f52
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-10-14 18:17:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1505,7 +1505,46 @@ client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name","BlackLight"));
     });
 
+client.on('message',async message => {
+    if(message.content.startsWith(prefix + "bbc")) {
+      let filter = m => m.author.id === message.author.id;
+      let thisMessage;
+      let thisFalse;
+      message.channel.send('🇧🇨| **ارسل الرسالة الان**').then(msg => {
 
+      let awaitM = message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 20000,
+        errors: ['time']
+      })
+      .then(collected => {
+        collected.first().delete();
+        thisMessage = collected.first().content;
+        msg.edit('🇧🇨| **هل انت متأكد؟**');
+        let awaitY = message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+          max: 1,
+          time: 20000,
+          errors: ['time']
+        })
+        .then(collected => {
+          if(collected.first().content === 'لا') {
+            msg.delete();
+            message.delete();
+            thisFalse = false;
+          }
+          if(collected.first().content === 'نعم') {
+            if(thisFalse === false) return;
+          message.guild.members.forEach(member => {
+            msg.edit('🇧🇨| **جاري الارسال**');
+            collected.first().delete();
+            member.send(`${thisMessage}\n\n${member} ,\nتم الارسال من : ${message.guild.name}\n تم الارسال بواسطة : ${message.author.tag}`);
+          });
+          }
+        });
+      });
+      });
+    }
+  });
 
 
 client.login(process.env.BOT_TOKEN);
