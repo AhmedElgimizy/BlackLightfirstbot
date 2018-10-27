75f9dde048ce564c11bc590c8e8deebd94e03e75
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-10-28 01:03:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '#'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Online `,"http://twitch.tv/S-F")
+client.user.setGame(online,"PLAYING")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
