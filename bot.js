1b653640a5ba3bd01200062922a2c21fc780fd03
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-11-22 22:00:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2521,4 +2521,19 @@ hours = 12;
   }
 });
 
+client.on('message', function(message) {
+    if(message.content.startsWith(prefix + 'roll')) {
+        let args = message.content.split(" ").slice(1);
+        if (!args[0]) {
+            message.channel.send('**Put a number**');
+            return;
+            }
+    message.channel.send(Math.floor(Math.random() * args.join(' ')));
+            if (!args[0]) {
+          message.edit('1')
+          return;
+        }
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
