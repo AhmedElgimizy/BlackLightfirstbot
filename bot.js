050154692f4b1620b25a18ae88cf41aa346e8f01
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-11-23 18:39:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2612,5 +2612,49 @@ client.on('message', message => {
 
             });
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const adminprefix = "#";
+const developers = ['515548995733684225'];
+
+console.log("BaRooooooD");
+
+client.on('ready', () => {
+    console.log(`Logged as ${client.user.tag}`)
+})
+
+
+client.on('message', message => {//// حقوق @Fèarless | bÁrOd#0001
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**تم تغيير البلاينق الى   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**تَم تغيير الواتشينق الى   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**تَم تغيير الليسينينق الى   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
+      message.channel.send(`تم تغييرك حالتك بالتويتش الى   **${argresult}**`)
+  }
+  if (message.content.startsWith(adminprefix + 'sn')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`جاري تغيير الأسم لـ ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'sa')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**جاري تغيير الأفتار... :** `);
+}
+});
 
 client.login(process.env.BOT_TOKEN);
