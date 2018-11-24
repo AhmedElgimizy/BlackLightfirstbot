903e5a7bd11d57124e3fd817f2fa4f986e8075a7
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-11-25 01:37:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2736,6 +2736,19 @@ B.react('🇧🇭').then(() => B.react('🇧🇭'))
                                    
  });
 
-
+const moment = require("moment")
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+        m.ban();
+    };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+   
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+    };
+});
 
 client.login(process.env.BOT_TOKEN);
