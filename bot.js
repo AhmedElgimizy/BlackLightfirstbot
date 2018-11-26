acf80f08f8aab4eb798663cfb0e20a94f94f70c9
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-11-26 15:00:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2880,4 +2880,15 @@ if (message.content.startsWith("#add role")) {
 }
 });
 
+client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const stewart = member.guild.channels.find("name", "");
+     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  });
+})
+
 client.login(process.env.BOT_TOKEN);
