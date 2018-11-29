b52a7d13c365a919f0f96c05167dd8d6f6538423
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-11-29 16:07:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2980,4 +2980,20 @@ if (message.content.startsWith(prefix + 'perms')) { // الامر
 });
 
 
+client.on('message', async message => {
+  if(message.content.startsWith(prefix + "js")) {
+  await  message.channel.send(`اكتب`)
+    let filter = m => m.author.id === message.author.id
+      var text = '';
+        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
+          .then(co => {
+            text = co.first().content
+
+              message.channel.send(`تم النشر`)
+                client.channels.get("517702509700317205").send(`${message.author.username}'s said => ${text}`)
+
+              })
+            }
+          })
+
 client.login(process.env.BOT_TOKEN);
