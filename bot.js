a367ab4a0e3f4bf00d29d10fafb087ee72d0cfbc
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-11-29 13:59:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2938,4 +2938,21 @@ client.on("message", message => {
 	} 
 });
 
+client.on('message', message => {
+    if (message.content.startsWith("#avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
