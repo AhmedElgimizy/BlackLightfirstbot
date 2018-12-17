070c83b967bf7ca24d048ff0f277ae7d5f0fd937
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-12-17 10:12:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3053,6 +3053,24 @@ client.on("message", message => {
       }
   });
       
-
+client.on("message", message => {
+  if(message.content.startsWith("#verfry")) { // الامر والبريفكس
+    let num = Math.floor((Math.random() * 4783) + 10);
+ 
+        message.channel.send(`**يرجاء كتابة الرقم التالي:** **${num}**`).then(m => {
+      message.channel.awaitMessages(res => res.content == `${num}`, {
+        max: 1,
+        time: 60000,
+        errors: ['time'],
+      }).then(collected => {
+        message.delete();
+        m.delete();
+        message.member.addRole(message.guild.roles.find(c => c.name == "Members")); // اسم الرتبة
+      }).catch(() => {
+        m.edit(`لقد أخذت وقتًا طويلاً لكتابة الرقم.  قم بإعادة كتابة الأمر مرة أخرى إذا كنت تريد التحقق من هويتك..`).then(m2 => m.delete(15000));
+      });
+    });
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
