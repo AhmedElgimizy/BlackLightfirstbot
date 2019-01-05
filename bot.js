2ac75c02d9ccd5e594bb63b55e502de921f6c4ba
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2019-01-05 16:51:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3148,6 +3148,26 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {//Toxic Codes
 
 });//Toxic Codes
 
+client.on('message', fras => {
+var prefix = "#";
+if(omar.content.split(' ')[0] == prefix + 'dc') {  // delete all channels يحذف جميع شاتات
+if (!omar.channel.guild) return;
+if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
+if(!omar.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**");
+omar.guild.channels.forEach(m => {
+m.delete();
+});
+}
+if(omar.content.split(' ')[0] == prefix + 'dr') { // delete all roles يحذف جميع رتب
+if (!omar.channel.guild) return;
+if(!omar.guild.member(omar.author).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**You Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
+if(!omar.guild.member(client.user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**I Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
+omar.guild.roles.forEach(m => {
+m.delete();
+});//Blacklight Codes 
+omar.reply("`تم حذف جميع الرتب بنجاح`")
+}//Blacklight Codes
+}); //BlackLight Codes
 
 
 
