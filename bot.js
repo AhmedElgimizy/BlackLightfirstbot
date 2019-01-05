ebbd274150c6f68f0d59e9dcbb80a739d94627e4
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2019-01-06 00:43:01 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "#"// البرفكس هنا
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('By Wenteed')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+
+  
+  
+
+
+});///الكود فوق ذي
+ 
+ 
+ 
+client.login(process.env.BOT_TOKEN);
