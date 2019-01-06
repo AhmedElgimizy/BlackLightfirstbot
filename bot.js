96f3487e9db488d0b8c4f575dc6346bf4c217d7f
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2019-01-06 15:09:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,33 @@ client.on('ready', () => {
   console.log('')
   console.log('')
 
+  client.on('guildMemberAdd', member => {
+  var embed = new Discord.RichEmbed()
+  .setTitle(" 😃 عضو جديد دخل السيرفر")
+  .setColor("RANDOM")
+  .addField("اسم العضو",`${member}`)
+  .addField("ايدي العضو",`${member.id}`)
+  .addField("رقم العضو",`${member.guild.memberCount}`)
+  .setThumbnail("http://www.ymcaswkansas.org/sites/ymcaswkansas.org/files/civicrm/friend.png")
+  var channel =member.guild.channels.find('name', 'welcome')
+if (!channel) return;
+      channel.send({embed : embed});
+});
+
+client.on('guildMemberRemove', member => {
+  var embed = new Discord.RichEmbed()
+  .setTitle("عضو غادر السيرفر")
+  .setColor("RANDOM")
+  .addField("اسم العضو",`${member}`)
+  .addField("ايدي العضو",`${member.id}`)
+  .addField("تبقى",`${member.guild.memberCount}`)
+  .setThumbnail("https://cdn.onlinewebfonts.com/svg/img_948.png")
+  var channel =member.guild.channels.find('name', 'welcome')
+  if (!channel) return;
+        channel.send({embed : embed});
+  });
+
+  
   
 });///الكود فوق ذي
  
