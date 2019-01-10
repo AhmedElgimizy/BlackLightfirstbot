336fcabb9a3ee5cf2aaf6ecbbade6345bcf78fa3
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2019-01-10 19:25:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,12 +19,46 @@ client.on('ready', () => {
   console.log(`Users! [ " ${client.users.size} " ]`);
   console.log(`channels! [ " ${client.channels.size} " ]`);
   console.log('╚[════════════════════════════════════]╝')
-  console.log('By Wenteed')
+  console.log('By LoonyMert')
   console.log('╔[════════════]╗')
   console.log(' Bot Is Online')
   console.log('╚[════════════]╝')
   console.log('')
 });///الكود فوق ذي
  
- 
+ const developers = ["408611591710310410", "347379495528824832"]
+const adminprefix = '$'
+client.on('message', message => {
+    var argresult = message.content.split(`).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(white_check_mark   ${argresult})
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(white_check_mark   ${argresult})
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(white_check_mark   ${argresult})
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+      message.channel.send(white_check_mark)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(Changing The Name To ..${argresult} )
+} else
+if (message.content.startsWith(adminprefix + 'setava')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(Changing The Avatar To :${argresult} `);
+}
+});
+
 client.login(process.env.BOT_TOKEN);
