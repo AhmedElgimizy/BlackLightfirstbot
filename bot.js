2584cdf73469b1d44e55495555f29a0022befc8b
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2019-01-13 15:48:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,6 +164,15 @@ client.on('message',async message => {
             }
 })
 
+client.on('message', message => {
+    if(message.content.includes('discord.gg')){
+                                            if(!message.channel.guild) return message.reply('** advertising me on DM ?    **');
+        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+        message.delete()
+    return message.reply(`** No Invite Links  !**`)
+    }
+}
+});
 
 
 
