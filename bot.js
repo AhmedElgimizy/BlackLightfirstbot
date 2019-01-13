dfb244326a8c1b39ce57aed0614b89385e46d1cf
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2019-01-13 15:57:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -223,7 +223,7 @@ if (message.content.toLowerCase().startsWith(prefix + `new`)) {
 if (message.content.toLowerCase().startsWith(prefix + `close`)) {
     if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
  
-    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
+    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`#confirm\`. This will time out in 10 seconds and be cancelled.`)
     .then((m) => {
       message.channel.awaitMessages(response => response.content === '#confirm', {
         max: 1,
