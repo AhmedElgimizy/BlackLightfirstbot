d9154f7880f0eb2689ff8d388205ae3339729398
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2019-01-13 16:31:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -362,5 +362,17 @@ client.on('message', message => {
 }
 });
 
+client.on("message", (message) => {
+    if (message.content.startsWith("${prefix}kick")) {
+      if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply('? ماعندك الصلاحيات');
+        var member= message.mentions.members.first();
+        member.kick().then((member) => {
+            message.channel.send(member.displayName + " مع السلامه :wave: ");
+        }).catch(() => {
+            message.channel.send("Error -_-");
+        });
+    }
+});
+
 
 client.login(process.env.BOT_TOKEN);
