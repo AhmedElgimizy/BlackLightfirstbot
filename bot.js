f506951eaa1bfe5bc155d3a8f2d4d60ca424bf61
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2019-01-13 16:29:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -283,7 +283,7 @@ client.on('message', message => {
   let args = message.content.split(" ").slice(1);
 
   if(!message.content.toLowerCase().startsWith(prefix)) return;
-  if(command == "#sugg") {
+  if(command == "sugg") {
     if(!args.join(" ")) return message.channel.send(`**يرجي كتابة الاقتراح **`);
     let channel = message.guild.channels.find(c => c.name == "suggestions");
     let embed = new Discord.RichEmbed()
