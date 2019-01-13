f18b578dfe845e04ced5f06f15ea988ec9eea36a
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2019-01-13 16:16:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -311,5 +311,17 @@ client.on('message',async msg => {//Toxic Codes
  
 });
 
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
+    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setColor('RANDOM')
+    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+var channel =member.guild.channels.find('name', 'welcome')
+if (!channel) return;
+channel.send({embed : embed});
+}); 
+
 
 client.login(process.env.BOT_TOKEN);
