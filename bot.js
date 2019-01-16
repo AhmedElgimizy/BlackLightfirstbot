f3da2271df227ada3b029afbb6293d784443199e
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2019-01-16 23:43:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -441,5 +441,17 @@ client.on('message', message => {
     }
     });
 
+client.on("message", (message) => {
+                        if (message.content.startsWith("#cv")) {
+                                    if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+                                let args = message.content.split(" ").slice(1);
+                            message.guild.createChannel(args.join(' '), 'text');
+                            message.channel.sendMessage('تـم إنـشاء روم كتابي')
+                            
+                        }
+                        });
+
+
+
 
 client.login(process.env.BOT_TOKEN);
